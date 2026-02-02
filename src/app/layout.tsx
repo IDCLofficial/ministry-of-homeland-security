e4ba23716ddafc5ee26e34a30138f845fd5719df
Cody998 <markhenryo368@gmail.com>
2026-02-02 01:46:22 +0100

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,46 +7,10 @@ import GlobalLoader from '@/components/GlobalLoader'
 import Providers from "./providers";
 
 
-
-// optimized metadata for seo
 export const metadata: Metadata = {
-  title: {
-    default: 'Imo State Ministry of Homeland Security and Vigilante Affairs',
-    template: '%s | Imo State Ministry of Homeland Security and Vigilante Affairs',
-  },
-  description: 'Official website of the Imo State Ministry of Homeland Security and Vigilante Affairs - overseeing and developing the state\'s security and vigilance sector, as well as managing community safety programs.',
-  keywords: ['Imo State Security', 'Ministry of Homeland Security', 'Vigilante Affairs Imo', 'Community Safety Imo'],
-  openGraph: {
-    type: 'website',
-    locale: 'en_NG',
-    url: 'https://homesecurity.im.gov.ng',
-    siteName: 'Imo State Ministry of Homeland Security and Vigilante Affairs',
-    title: 'Imo State Ministry of Homeland Security and Vigilante Affairs',
-    description: 'Official website of the Imo State Ministry of Homeland Security and Vigilante Affairs - overseeing and developing the state\'s security and vigilance sector, as well as managing community safety programs.',
-    images: [
-      {
-        url: 'https://homesecurity.im.gov.ng/images/IMSG-Logo.svg',
-        width: 1200,
-        height: 630,
-        alt: 'Imo State Ministry of Homeland Security and Vigilante Affairs',
-      }
-    ],
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  verification: {
-    google: '_7c1t_i-UsjS1_BD_kE-vsXMUak-mgMUnIT91dEUEQ4',
-  },
-}
+  title: "Ministry of Homeland Security and Vigilante Affairs",
+  description: "The Ministry of Homeland Security and Vigilante Affairs in Imo State is responsible for overseeing and developing the state's security and vigilance sector, as well as managing community safety programs.",
+};
 
 export default function RootLayout({
   children,
